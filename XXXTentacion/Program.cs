b909d06b5a5618b345c517118e218714b89737be
fif <fif@DESKTOP-BFHUIKE>
2022-11-09 08:56:04 +1200

    Добавьте файлы проекта.

---
+++ XXXTentacion/Program.cs
@@ -0,0 +1,10 @@
+﻿//Скрипников Серге 17 вар 3.2
+Console.WriteLine("Введите натуральное число:");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= x; i++)
+{
+    if ((x % i) == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
